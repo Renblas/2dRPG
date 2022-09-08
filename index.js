e80989a3e04d2fbdbd82f6f422195f2217ed0d6e
Renblas <77644615+Renblas@users.noreply.github.com>
2022-09-08 10:25:15 -0400

    hi

---
+++ index.js
@@ -0,0 +1,18 @@
+/* Main Variables */
+
+
+// Initial Setup
+function setup() {
+    createCanvas(windowWidth, windowHeight)
+}
+
+// Draw Loop
+function draw() {
+    background(200)
+    text("helloWorld", 0, 0)
+}
+
+
+// load Game
+
+
